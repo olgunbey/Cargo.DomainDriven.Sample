1d2ae60af47ccc015293f243d3ba7f91791143a1
olgunbey <olgunsahin0161@hotmail.com>
2025-07-06 18:10:07 +0300

    feat: added event sourcing  for Cargo information

---
+++ DomainDriven.Sample.API/CargoManagement/Domain/Events/CargoInformationEvent.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace DomainDriven.Sample.API.CargoManagement.Domain.Events
+{
+    public record CargoInformationEvent(
+    int CompanyId,
+    int CustomerId,
+    int OrderId,
+    string Status,
+    DateTime CargoCreatedDate
+) : INotification;
+
+}
