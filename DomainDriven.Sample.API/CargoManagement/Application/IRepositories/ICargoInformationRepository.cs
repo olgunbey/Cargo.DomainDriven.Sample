6cd38d3e3e6c2f80d60d9c25588b59d611cf1afa
olgunbey <olgunsahin0161@hotmail.com>
2025-07-06 16:10:51 +0300

    feat: implementation IApplicationDbContext

---
+++ DomainDriven.Sample.API/CargoManagement/Application/IRepositories/ICargoInformationRepository.cs
@@ -0,0 +1,9 @@
+﻿using DomainDriven.Sample.API.CargoManagement.Domain.Aggregates;
+
+namespace DomainDriven.Sample.API.CargoManagement.Application.IRepositories
+{
+    public interface ICargoInformationRepository
+    {
+        public Task<CargoInformation> AddCargoInformation(CargoInformation cargoInformation);
+    }
+}
--- DomainDriven.Sample.API/Database/CargoDbContext.cs
+++ DomainDriven.Sample.API/Database/CargoDbContext.cs
@@ -2,7 +2,14 @@
 
 namespace DomainDriven.Sample.API.Database
 {
-    public class CargoDbContext : DbContext
+    public class CargoDbContext : DbContext, IApplicationDbContext
     {
+        public CargoDbContext(DbContextOptions<CargoDbContext> dbContextOptions) : base(dbContextOptions)
+        {
+        }
+        DbSet<TEntity> IApplicationDbContext.GetEntity<TEntity>()
+        {
+            return Set<TEntity>();
+        }
     }
 }
