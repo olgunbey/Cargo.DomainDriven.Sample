70ace610e7d2f46aa84538cff4e32f1265bb9fc1
olgunbey <olgunsahin0161@hotmail.com>
2025-07-08 11:49:45 +0300

    feat:  Added event Assigned cargo to emplooye from struct

--- DomainDriven.Sample.API/CargoManagement/Domain/Events/CargoInformationNotification.cs
+++ DomainDriven.Sample.API/CargoManagement/Domain/Events/CargoInformationNotification.cs
@@ -7,6 +7,7 @@ namespace DomainDriven.Sample.API.CargoManagement.Domain.Events
     int CustomerId,
     int OrderId,
     string Status,
+    string cargoCode,
     DateTime CargoCreatedDate,
     DateTime? CargoUpdatedDate
 ) : INotification;
---
+++ DomainDriven.Sample.API/Common/CargoInformationEvent.cs
@@ -0,0 +1,13 @@
+﻿namespace DomainDriven.Sample.API.Common
+{
+    public record CargoInformationEvent(
+    int CompanyId,
+    int CustomerId,
+    int OrderId,
+    string Status,
+    string CargoCode,
+    int EmplooyeId,
+    DateTime CargoCreatedDate,
+    DateTime? CargoUpdatedDate
+);
+}
