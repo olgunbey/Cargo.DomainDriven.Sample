adf7b877c44f32c6f6a59d1efdbaf3785b5a81d2
olgunbey <olgunsahin0161@hotmail.com>
2025-07-13 20:22:29 +0300

    feat: added GetAllCityAndDistrict query

---
+++ DomainDriven.Sample.API/Location/Application/Dtos/GetAllCityAndDistrictResponseDto.cs
@@ -0,0 +1,14 @@
+﻿namespace DomainDriven.Sample.API.Location.Application.Dtos
+{
+    public class GetAllCityAndDistrictResponseDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<District> District { get; set; }
+    }
+    public class District
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
