78d2137ff601c14216ee541a6d8af2bc3f32eedc
olgunbey <olgunsahin0161@hotmail.com>
2025-07-22 20:25:47 +0300

    feat: Added AddProduct handler to Product bounded context

---
+++ DomainDriven.Sample.API/Feature/Product/Application/Commands/AddProduct.cs
@@ -0,0 +1,55 @@
+﻿using DomainDriven.Sample.API.Common;
+using DomainDriven.Sample.API.Feature.Product.Application.Interfaces;
+using DomainDriven.Sample.API.Feature.Product.Domain.Aggregates;
+using DomainDriven.Sample.API.Feature.Product.Domain.Interfaces;
+using MediatR;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace DomainDriven.Sample.API.Feature.Product.Application.Commands
+{
+    public class AddProductRequest : IRequest<ResponseDto<NoContentDto>>
+    {
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductAttribute { get; set; }
+
+    }
+    public class AddProductRequestHandler(IProductDbContext productDbContext, IProduct product, IMongoClient mongoClient, IProductAttribute productAttribute) : IRequestHandler<AddProductRequest, ResponseDto<NoContentDto>>
+    {
+        public async Task<ResponseDto<NoContentDto>> Handle(AddProductRequest request, CancellationToken cancellationToken)
+        {
+            var generateProductAttribute = productAttribute.GenerateProductAttribute(request.ProductAttribute.ToBsonDocument());
+
+            var mongoDatabase = mongoClient.GetDatabase("DomainDrivenSample");
+
+            await mongoDatabase.GetCollection<ProductAttribute>("ProductAttribute")
+                 .InsertOneAsync(generateProductAttribute);
+
+            var generateProduct = product.GenerateProduct(request.Name,
+               request.Stock,
+               request.Price,
+               request.CategoryId,
+               generateProductAttribute.Id);
+
+            productDbContext.Product.Add(generateProduct);
+            using (var session = await mongoClient.StartSessionAsync())
+            {
+                session.StartTransaction();
+                try
+                {
+                    await productDbContext.SaveChangesAsync(cancellationToken);
+                    await session.CommitTransactionAsync();
+                }
+                catch (Exception e)
+                {
+                    await session.AbortTransactionAsync();
+                    return ResponseDto<NoContentDto>.Fail("Hata!!");
+                }
+            }
+            return ResponseDto<NoContentDto>.Success(201);
+        }
+    }
+}
---
+++ DomainDriven.Sample.API/Feature/Product/Domain/Interfaces/IProduct.cs
@@ -0,0 +1,7 @@
+﻿namespace DomainDriven.Sample.API.Feature.Product.Domain.Interfaces
+{
+    public interface IProduct
+    {
+        public Aggregates.Product GenerateProduct(string name, int stock, decimal price, int categoryId, Guid productAttributeId);
+    }
+}
